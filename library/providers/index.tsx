0e449a3bf10a9d35e28ebe6cb967b384019a9055
kelvinpraises <kelvinpraises@gmail.com>
2024-06-02 07:49:19 +0100

    chore: remove wagmi provider

--- library/providers/index.tsx
+++ library/providers/index.tsx
@@ -1,16 +1,14 @@
 "use client";
+
 import React from "react";
 import ConnectKitProvider from "./connectkit";
 import WagmiProvider from "./wagmi";
-import SIWEProvider from "./siwe";
 
 const RootProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="w-full">
       <WagmiProvider>
-        <SIWEProvider>
-          <ConnectKitProvider>{children}</ConnectKitProvider>
-        </SIWEProvider>
+        <ConnectKitProvider>{children}</ConnectKitProvider>
       </WagmiProvider>
     </div>
   );
