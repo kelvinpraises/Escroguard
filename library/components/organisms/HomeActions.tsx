e6b100446e2c18e3712c22cf70b7bbfe65ba5edb
kelvinpraises <kelvinpraises@gmail.com>
2024-06-03 01:39:28 +0100

    chore: simplify component

--- library/components/organisms/HomeActions.tsx
+++ library/components/organisms/HomeActions.tsx
@@ -1,130 +1,104 @@
 "use client";
-import useHomeActions from "../../hooks/homeActions";
-import { HomeAction } from "../../type";
 
-const HomeActions = ({ action }: { action: HomeAction | null }) => {
-  // const {
-  //   values,
-  //   updateValues,
-  //   joinSwap,
-  //   deploySwap,
-  //   isLoading,
-  //   setIsLoading,
-  // } = useHomeActions();
+import useHomeActions from "@/hooks/homeActions";
+import { HomeAction } from "../../type";
+import Input from "../atoms/Input";
+
+interface HomeActionsProps {
+  action: HomeAction | null;
+}
+
+const HomeActions = ({ action }: HomeActionsProps) => {
+  const { values, updateValues, joinSwap } = useHomeActions();
+
+  const renderContent = () => {
+    switch (action) {
+      case "create":
+        return (
+          <>
+            <p className="text-white text-5xl">Create Swap</p>
+            <div className="flex flex-col gap-8">
+              <Input
+                label="Swap name"
+                value={values.swapName}
+                onChange={(e) => updateValues({ swapName: e.target.value })}
+              />
+              <Input
+                type="number"
+                label="Swap fees"
+                value={values.swapFees.toString()}
+                onChange={(e) =>
+                  updateValues({ swapFees: parseFloat(e.target.value) })
+                }
+              />
+              <button className="py-4 w-full text-white rounded-[5px] bg-black">
+                Create Private Swap
+              </button>
+            </div>
+          </>
+        );
+
+      case "join":
+        return (
+          <>
+            <p className="text-white text-5xl">Join Swap</p>
+            <div className="flex flex-col gap-8">
+              <Input
+                label="Swap ID"
+                value={values.swapId}
+                onChange={(e) => updateValues({ swapId: e.target.value })}
+              />
+              <button
+                onClick={() => joinSwap(values.swapId)}
+                className="py-4 w-full text-white rounded-[5px] bg-black"
+              >
+                Request Swap Access
+              </button>
+            </div>
+          </>
+        );
+
+      case "joined":
+        return (
+          <>
+            <p className="text-white text-5xl">Joined Swaps</p>
+            <div className="flex flex-col gap-8">
+              {values.joined.length ? (
+                values.joined.map((item, i) => (
+                  <div
+                    key={i}
+                    className="flex gap-4 p-4 bg-black rounded-[5px] items-center"
+                  >
+                    <div className="flex flex-col gap-4 w-min">
+                      <p className="text-white font-normal text-xl">
+                        {item.name}
+                      </p>
+                      <p className="text-white font-normal">
+                        {item.contractAddress}
+                      </p>
+                    </div>
+                    <button className="w-full grid place-items-center">
+                      <img src="LookUp.svg" alt="Look Up" />
+                    </button>
+                  </div>
+                ))
+              ) : (
+                <p className="text-white">
+                  You haven't joined a swap space yet
+                </p>
+              )}
+            </div>
+          </>
+        );
+
+      default:
+        return null;
+    }
+  };
 
   return (
-    <div className=" max-w-[450px] w-full p-8 flex flex-col gap-[64px]">
-      {(() => {
-        let item;
-        switch (action) {
-          case "create":
-            item = (
-              <>
-                <p className=" text-white text-5xl">Create Swap</p>
-                <div className=" flex flex-col gap-8">
-                  <div className=" flex flex-col gap-4">
-                    <p>Swap name</p>
-                    <input
-                      type="text"
-                      className="  h-[60px] rounded-[5px] bg-[#1F2329] outline-none px-2"
-                      value={values.swapName}
-                      onChange={(e) =>
-                        updateValues({ swapName: e.target.value })
-                      }
-                    />
-                    <p>Swap fees</p>
-                    <input
-                      type="text"
-                      className="  h-[60px] rounded-[5px] bg-[#1F2329] outline-none px-2"
-                      value={values.swapFees}
-                      onChange={(e) =>
-                        updateValues({ swapFees: e.target.value })
-                      }
-                    />
-                  </div>
-                  <button
-                    disabled={!deploySwap || isLoading}
-                    onClick={() => {
-                      deploySwap?.();
-                      setIsLoading(true);
-                    }}
-                    className=" py-4 w-full text-white rounded-[5px] bg-black"
-                  >
-                    {isLoading ? "CREATING..." : "CREATE"}
-                  </button>
-                </div>
-              </>
-            );
-            break;
-
-          case "join":
-            item = (
-              <>
-                <p className=" text-white text-5xl">Join Swap</p>
-                <div className=" flex flex-col gap-8">
-                  <div className=" flex flex-col gap-4">
-                    <div className=" flex flex-col gap-4">
-                      <p>Swap ID</p>
-                      <input
-                        type="text"
-                        className="  h-[60px] rounded-[5px] bg-[#1F2329] outline-none px-2"
-                        value={values.swapId}
-                        onChange={(e) =>
-                          updateValues({ swapId: e.target.value })
-                        }
-                      />
-                    </div>
-                  </div>
-                  <button
-                    onClick={() => joinSwap(values.swapId)}
-                    className=" py-4 w-full text-white rounded-[5px] bg-black"
-                  >
-                    Join
-                  </button>
-                </div>
-              </>
-            );
-            break;
-
-          case "joined":
-            item = (
-              <>
-                <p className=" text-white text-5xl">Joined Swaps</p>
-                <div className=" flex flex-col gap-8">
-                  {values.joined.length > 0 ? (
-                    values.joined.map((items, i) => (
-                      <div
-                        className=" flex gap-4 p-4 bg-black rounded-[5px] items-center"
-                        key={i}
-                      >
-                        <div className=" flex flex-col gap-4 w-min">
-                          <p className=" text-white font-normal text-xl">
-                            {items.name}
-                          </p>
-                          <p className=" text-white font-normal">
-                            {items.contractAddress}
-                          </p>
-                        </div>
-                        <button className=" w-full grid place-items-center">
-                          <img src="LookUp.svg" alt="" />
-                        </button>
-                      </div>
-                    ))
-                  ) : (
-                    <>You haven't joined a swap space yet</>
-                  )}
-                </div>
-              </>
-            );
-            break;
-
-          default:
-            item = <></>;
-            break;
-        }
-
-        return item;
-      })()}
+    <div className="max-w-[450px] w-full p-8 flex flex-col gap-16">
+      {renderContent()}
     </div>
   );
 };
