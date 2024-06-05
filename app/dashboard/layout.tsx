96e89dd8c0041a1cfb9c8ef875b4d0c5b56b693d
kelvinpraises <kelvinpraises@gmail.com>
2024-06-05 23:08:56 +0100

    chore: add components

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,9 @@
+import DashboardLayoutWrapper from "@/components/template/DashboardLayoutWrapper";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <DashboardLayoutWrapper>{children}</DashboardLayoutWrapper>;
+}
---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,9 @@
+import LoadingComponent from "@/components/atoms/Loading";
+
+export default function Loading() {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <LoadingComponent />
+    </div>
+  );
+}
