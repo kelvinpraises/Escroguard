6ba5e507049ee9dec838897cec2b1330affd5bb6
kelvinpraises <kelvinpraises@gmail.com>
2024-06-07 08:34:19 +0100

    feat: export

--- library/services/magic/index.ts
+++ library/services/magic/index.ts
@@ -1,3 +1,4 @@
-export { magicClient } from "./magicClient";
-export { magicAdmin } from "./magicAdmin";
+export { RPCError, RPCErrorCode } from "magic-sdk";
 export type { MagicUserMetadata } from "magic-sdk";
+export { magicAdmin } from "./magicAdmin";
+export { magicClient } from "./magicClient";
